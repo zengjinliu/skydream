9ab8917de48ef7ad460efc6d93aab243e58cff94
zengjinliu <zengjin_job@163.com>
2020-09-10 14:25:10 +0800

    时间工具类新增一些方法

--- src/main/java/comskydream/cn/skydream/utils/DateUtils.java
+++ src/main/java/comskydream/cn/skydream/utils/DateUtils.java
@@ -1,7 +1,13 @@
 package comskydream.cn.skydream.utils;
 
+import org.apache.tomcat.jni.Local;
+
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.time.temporal.TemporalField;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * @author Jayson
@@ -36,6 +42,72 @@ public class DateUtils {
     public static long getSecond(LocalDateTime localDateTime){
         Instant instant = LocalDateTime.now().toInstant(ZoneOffset.of("+8"));
         return instant.getEpochSecond();
+    }
+
+    /**
+     * localDate transfer Date
+     * @param localDate
+     * @return Date
+     */
+    public static Date localDate2Date(LocalDate localDate){
+        Instant instant = localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+        Date date = Date.from(instant);
+        return date;
+    }
+
+    /**
+     *  Date transfer localDate
+     * @param date
+     * @return LocalDate
+     */
+    public static LocalDate date2LocalDate(Date date){
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return localDate;
+    }
+
+    /**
+     * Date transfer LocalDateTime
+     * @param date
+     * @return LocalDateTime
+     */
+    public static LocalDateTime date2LocalDateTime(Date date){
+        LocalDateTime localDateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        return localDateTime;
+    }
+
+    /**
+     * Date transfer LocalDateTime
+     * @param localDateTime
+     * @return LocalDateTime
+     */
+    public static Date localDateTime2Date(LocalDateTime localDateTime){
+        Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+        Date date = Date.from(instant);
+        return date;
+    }
+
+    /**
+     *  dateStr must adjust to pattern transfer to LocalDate
+     * @param dateStr 时间格式字符串
+     * @param pattern 格式化 eq yyyy-MM-dd
+     * @return
+     */
+    public static LocalDate str2LocalDate(String dateStr,String pattern){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, Locale.CHINA);
+        LocalDate localDate = LocalDate.parse(dateStr, formatter);
+        return localDate;
+    }
+
+    /**
+     *  dateStr must adjust to pattern transfer to LocalDate
+     * @param dateStr 时间格式字符串
+     * @param pattern 格式化 eq yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static LocalDateTime str2LocalDateTime(String dateStr,String pattern){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, Locale.CHINA);
+        LocalDateTime parse = LocalDateTime.parse(dateStr, formatter);
+        return parse;
     }
 
 
--- src/test/java/comskydream/cn/skydream/SkyDreamApplicationTests.java
+++ src/test/java/comskydream/cn/skydream/SkyDreamApplicationTests.java
@@ -2,11 +2,14 @@ package comskydream.cn.skydream;
 
 import comskydream.cn.skydream.entity.SysMenu;
 import comskydream.cn.skydream.service.SysMenuService;
+import comskydream.cn.skydream.utils.DateUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
@@ -19,7 +22,16 @@ class SkyDreamApplicationTests {
     void getNavMenuList() {
         List<SysMenu> sysMenus = sysMenuService.queryAllMenu("1");
         Assert.notNull(sysMenus, "this list must not be null");
+    }
 
+    @Test
+    public void test1(){
+        Date date = DateUtils.localDate2Date(LocalDate.now());
+        System.out.println(date);
+        System.out.println(DateUtils.date2LocalDate(date));
+        String dataStr = "2020-09-10";
+        LocalDate localDate = DateUtils.str2LocalDate(dataStr, "yyyy-MM-dd");
+        System.out.println(localDate);
     }
 
 }
