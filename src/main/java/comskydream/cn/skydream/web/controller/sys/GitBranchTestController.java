c8738c05abcfb2d5949060c1431e9dc3b66f2a97
liuzengjin <zengjin_liu@aliyun.com>
2022-09-06 15:19:19 +0800

    -git branch test

---
+++ src/main/java/comskydream/cn/skydream/web/controller/sys/GitBranchTestController.java
@@ -0,0 +1,21 @@
+package comskydream.cn.skydream.web.controller.sys;
+
+import comskydream.cn.skydream.common.ResultJson;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lzj
+ * @date 2022/9/6
+ */
+@RestController
+public class GitBranchTestController {
+
+
+
+    @GetMapping("/hello")
+    public ResultJson hello(){
+        return ResultJson.success();
+    }
+
+}
